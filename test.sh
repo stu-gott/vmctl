85df3c01608619e87e7945b979b02a9c98cfb1eb
Fabian Deutsch <fabiand@fedoraproject.org>
2018-09-21 10:38:09 +0200

    ci: Verify that VMs and VMIs will be scheduled
    Previously testing was lame.
    Now we ensure basic functionality by checking if the number of scaled VMs is scheduled.
    
    This change also fixes the build
    
    Signed-off-by: Fabian Deutsch <fabiand@fedoraproject.org>

--- test.sh
+++ test.sh
@@ -1,15 +1,29 @@
 #!/bin/bash
 
+set -e
+
+kubectl apply -f demo/manifests/vm.yaml
+
 kubectl create rolebinding default-admin \
   --clusterrole=cluster-admin \
   --serviceaccount=default:default \
-  --namespace default
+  --namespace default || :
 
 kubectl apply \
   -f manifests/deployment.yaml
 
-
 kubectl get deployment | grep vmctl
 
-kubectl scale --replicas=1 deployment/vmctl
-kubectl get deployments | egrep "vmctl\s+1"
+for SCALE in 1 2 3 2 1;
+do
+  kubectl scale --replicas=$SCALE deployment/vmctl
+  kubectl get deployments | egrep "vmctl\s+$SCALE"
+
+  kubectl wait --for condition=available deployment vmctl
+
+  kubectl get vm,vmi
+
+  sleep 4.2  # Give some time to schedule the VMI
+
+  test $(kubectl get vmi | grep testvm-vmctl | wc -l ) -eq $SCALE
+done
