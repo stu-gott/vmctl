5cda8d89c8aaec3a7a11239f4e501fc580ea1f27
Fabian Deutsch <fabiand@fedoraproject.org>
2018-10-05 09:58:30 +0200

    Set a nodeSelector
    This will schedule any VMI alongside the corresponding vmctl pod
    
    Fixes #1
    
    Signed-off-by: Fabian Deutsch <fabiand@fedoraproject.org>

--- cmd/vmctl/derive_vm.js
+++ cmd/vmctl/derive_vm.js
@@ -1,31 +1,13 @@
 prototypeVmFile = arguments[0]
 instanceName = arguments[1]
+nodeName = arguments[2]
 
 vm = JSON.parse(read(prototypeVmFile))
 
 delete vm.metadata.ownerReferences
 vm.metadata.name = instanceName
 vm.spec.running = true
-/*vm.spec.template.spec.affinity = {
-  "podAffinity": {
-    "requiredDuringSchedulingIgnoredDuringExecution": [
-      {
-        "labelSelector": {
-          "matchExpressions": [
-            {
-              "key": "guest",
-              "operator": "In",
-              "values": [
-                instanceName
-              ]
-            }
-          ]
-        },
-        "topologyKey": "kubernetes.io/hostname"
-      }
-    ]
-  }
-}*/
+vm.spec.template.spec.nodeSelector = {"kubernetes.io/hostname": nodeName}
 vm.status = {}
 
 print(JSON.stringify(vm))
